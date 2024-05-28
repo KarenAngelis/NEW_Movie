467caf027e1b16f52812671323dc8cf2af95df61
Karen <karen.denich@gmail.com>
2024-05-28 12:25:39 -0300

    SignIn

--- vite-project/src/hooks/auth.jsx
+++ vite-project/src/hooks/auth.jsx
@@ -1,11 +1,34 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { api } from "../service/api";
 
 export const AuthContext = createContext({});
 
 
 function AuthProvider({ children }) {
+
+  const [data, setData] = useState({});
+  async function signIn({ email, password }) {
+    
+    try {
+    const response = await api.post("/sessions", { email, password });
+    const { token, user } = response.data;
+
+      api.defaults.headers.athorization = `Bearer ${token}`;
+      setData({ token, user });
+
+    } catch (error) {
+      if(error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível entrar.");
+      }
+    }
+  }
+
+
+
   return (
-  <AuthContext.Provider value={{ name: 'Rodrigo', email: 'rodrigo@email.com' }}>
+  <AuthContext.Provider value={{ signIn, user: data.user }}>
     { children }
   </AuthContext.Provider>
   )
